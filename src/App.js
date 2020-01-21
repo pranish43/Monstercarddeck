e991d69e0aa6003acbe487b424ef4e77423d195e
pranish43 <pranishkarki43@gmail.com>
2020-01-20 22:43:27 -0600

    added card

--- src/App.js
+++ src/App.js
@@ -18,11 +18,7 @@ componentDidMount(){
   render(){
   return (
     <div className="App">
-    <CardList name='pranish'>
-    {
-      this.state.monsters.map(monster=><h1 key={monster.id}>{monster.name}</h1>)
-    }
-    </CardList>
+    <CardList monsters={this.state.monsters}/>
     </div>
   );
   }
